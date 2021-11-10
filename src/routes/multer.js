9cfb0059f0fe0564be091c0be2462835157ae168
henry <merchanha@gmail.com>
2021-11-10 16:54:07 -0300

    multer

---
+++ src/routes/multer.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+const multer = require('multer')
+const conn = require('../database/db')
+
+const storage = multer.diskStorage({
+    destination: (req, file, cd) => {
+        cd(null, "public")
+    },
+    filename: (req, file, cb) => {
+        const extension = file.mimetype.split("/")[1] //  image/jpg, image/gif = ['image', 'jpg']
+        const name = file.originalname.split(".")[0]
+        cb(null, `images/${name}_${Date.now()}.${extension}` ) 
+
+    }
+})
+const fileManager = multer({storage: storage})
+
+router.post('/fotos',  fileManager.single('avatar'), async (req, res) =>{
+    
+    let user_id = 17;
+    const [result, cfieldds]  = await  conn(`update users set imagen = ? `, [req.file.filename])
+
+    res.send('recibido')
+})  
--- src/routes/user.js
+++ src/routes/user.js
@@ -18,20 +18,52 @@ router.post('/user', async (req, res) => {
     const user = req.body.user;
     const password = req.body.password
 
-    
+
 
     if (!password.lenght < 8) {
-        res.json({error: 'Password muy corto'})
+        res.json({ error: 'Password muy corto' })
     }
 
-    const hashed_pass =  await hashearPassword(password)    
+    const hashed_pass = await hashearPassword(password)
 
-    con.query(`INSERT into users (name, user, password) values(?,?,?)`, [name, user, hashed_pass], (err, result)=>{
+    con.query(`INSERT into users (name, user, password) values(?,?,?)`, [name, user, hashed_pass], (err, result) => {
         console.log(err)
     })
 
- 
-           
+
+
+});
+
+
+router.post('/Login', async (req, res) => {
+
+    const user = req.body.user;
+    const password = req.body.password
+    
+
+
+
+    con.query(`SELECT * FROM users WHERE user = ? AND password = ?`,
+        [user, password],
+        (err, result) => {
+
+            if (err) {
+                res.send({ err: err })
+
+            }
+
+
+            if (result) {
+                console.log(result);
+            } else {
+                res.send({ message: "No existe el usuario con esa combinacion Usuario/Contraseña" })
+            }
+        }
+
+    )
+
+
+
 });
 
 
