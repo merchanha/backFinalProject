9a1638d37f722ee0731d926da831aa7ebc2817b9
henry <merchanha@gmail.com>
2021-11-11 17:15:10 -0300

    multer

--- src/routes/multer.js
+++ src/routes/multer.js
@@ -17,9 +17,14 @@ const storage = multer.diskStorage({
 const fileManager = multer({storage: storage})
 
 router.post('/fotos',  fileManager.single('avatar'), async (req, res) =>{
+
+    const nombre = req.body.nombre;
+    const apellido = req.body.apellido;
+    const telefono = req.body.apellido;
+    const imagen = req.file.filename;
+    const comentarios = req.body.comentarios;
     
-    let user_id = 17;
-    const [result, cfieldds]  = await  con(`update users set imagen = ? `, [req.file.filename])
+    const [result, cfieldds]  = await  con(`INSERT into consultas (nombre, apellido, telefono, imagen, comentarios) values(?,?,?,?,?)`, [nombre, apellido, telefono,imagen,comentarios], [req.file.filename])
 
     res.send('recibido')
 })  
